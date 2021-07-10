11c9eff710e7ecbae4c45ebfc94935f2c2e55d5f
Palash <palashnayek111@gmail.com>
2021-07-10 11:56:39 +0530

    Splash Complete

--- app/src/main/java/com/palash/stockvisorapp/Activities/SplashActivity.kt
+++ app/src/main/java/com/palash/stockvisorapp/Activities/SplashActivity.kt
@@ -1,21 +1,24 @@
 package com.palash.stockvisorapp.Activities
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.palash.stockvisorapp.MainActivity
+import com.palash.stockvisorapp.NetworkStatusCheck.Internet
 import com.palash.stockvisorapp.R
 
-
 class SplashActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         val imageView: ImageView = findViewById(R.id.loadingImg)
         Glide.with(this).load(R.drawable.loading).into(imageView)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
 
     }
 }
